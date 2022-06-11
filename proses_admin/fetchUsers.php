adce12576aad49e229b2ccd3450fb9ecca87abfd
DimensionalDragon <andhikangurah@gmail.com>
2022-06-12 05:16:45 +0800

    Change searchbar to also search for usernames

--- proses_admin/fetchUsers.php
+++ proses_admin/fetchUsers.php
@@ -7,10 +7,10 @@ $sql = "SELECT username, nama_karyawan AS nama, jabatan AS status FROM tbl_admin
 if(isset($_GET['search'])) {
     $search = $_GET['search'];
     $sql = "SELECT username, nama_karyawan AS nama, jabatan AS status FROM tbl_admin
-            WHERE nama_karyawan LIKE '%$search%'
+            WHERE nama_karyawan LIKE '%$search%' OR username LIKE '%$search%'
             UNION
             SELECT username, nama_tamu, 'pelanggan' FROM tbl_pelanggan
-            WHERE nama_tamu LIKE '%$search%'";
+            WHERE nama_tamu LIKE '%$search%' OR username LIKE '%$search%'";
 }
 $query = mysqli_query($con, $sql);
 $no = 1;
@@ -23,4 +23,3 @@ while ($data = mysqli_fetch_assoc($query)):
     <td><?php echo $data['status']; ?></td>
 </tr>
 <?php endwhile; ?>
-?>
