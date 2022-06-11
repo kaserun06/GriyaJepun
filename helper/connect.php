fe2d267e5adace27ec6911101af96620ac47cc64
DimensionalDragon <andhikangurah@gmail.com>
2022-06-11 21:44:54 +0800

    Change database connections info and Add Heroku related files

--- helper/connect.php
+++ helper/connect.php
@@ -1,6 +1,6 @@
 <?php
 include 'constant.php';
-$con = mysqli_connect(HOST,UNAME,PASS,DB);
+$con = mysqli_connect(HOST, UNAME, PASS, DB, PORT);
   if (mysqli_connect_errno()){
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
--- helper/constant.php
+++ helper/constant.php
@@ -1,7 +1,8 @@
 <?php
 // Connection Var
-define("HOST", "localhost"); // MySQL url
-define("UNAME", "root"); // MySQL's username
-define("PASS", ""); // MySQL's password 
-define("DB", "db_jepun"); // DB's name 
+define("HOST", "b72dwwamav0qiynn3pni-mysql.services.clever-cloud.com"); // MySQL url
+define("UNAME", "utzjqobl7etmx1op"); // MySQL username
+define("PASS", "RW5tRC5U4FQSQ2ZxUjPf"); // MySQL password 
+define("DB", "b72dwwamav0qiynn3pni"); // DB name 
+define("PORT", 3306); //MySQL port
 ?>
